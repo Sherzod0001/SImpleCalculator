40056ca8ea6f588dbb1c2c1941d66ef2e8f8a67b
Sherzod0001 <141539590+Sherzod0001@users.noreply.github.com>
2024-06-09 22:58:56 +0500

    add methods

--- Program.cs
+++ Program.cs
@@ -1,38 +1,37 @@
 ﻿using System;
 
 Console.WriteLine("Welcome to Calculator Application");
+
 while (true)
 {
     try
     {
         Console.Write("Enter first number: ");
-        int firstNumber = Convert.ToInt32(Console.ReadLine());
+        string userInputOne = Console.ReadLine();
+        int firstNumber = Convert.ToInt32(userInputOne);
         Console.WriteLine("Converting.....");
 
-
         Console.Write("Enter second number: ");
-        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        string userInputTwo = Console.ReadLine();
+        int secondNumber = Convert.ToInt32(userInputTwo);
         Console.WriteLine("Converting.....");
         Console.WriteLine("");
         Console.WriteLine("Calculation process completed.....");
         Console.WriteLine("");
 
-        Console.WriteLine("Results:");
-        Console.WriteLine($"When added: {firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
-        Console.WriteLine($"When subtracted: {firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
-        Console.WriteLine($"When multiplied: {firstNumber}  {secondNumber} = {firstNumber * secondNumber}");
-        Console.WriteLine($"When divided: {firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
-        Console.WriteLine($"When moduluid: {firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
-        Console.WriteLine($"{firstNumber} power of {secondNumber} = {Math.Pow(firstNumber, secondNumber)}");
-        Console.WriteLine($"When rooting: The square root of {firstNumber} is {Math.Sqrt(firstNumber)}");
-        Console.WriteLine($"When rooting: The square root of {secondNumber} is {Math.Sqrt(secondNumber)}");
+        AddingMethod(firstNumber, secondNumber);
+        SubtractingMethod(firstNumber, secondNumber);
+        MultipleyingMethod(firstNumber, secondNumber);
+        DividingMethod(firstNumber, secondNumber);
+        SquareRootMethod(firstNumber, secondNumber);
+        PowMethod(firstNumber, secondNumber);
     }
     catch (Exception e)
     {
-        Console.WriteLine("the value you entered cannot be converted to a number !");
+        Console.WriteLine("The value you entered cannot be converted to a number !");
         Console.WriteLine("Please enter a number !");
-
     }
+
     Console.WriteLine("Do you want to use the program again? 👉 y/n 👈");
     string answer = Console.ReadLine();
 
@@ -44,4 +43,36 @@ while (true)
     {
         Console.WriteLine("Thank you for using the program 🤝");
     }
+}
+
+static void AddingMethod(int firstNumber, int secondNumber)
+{
+    Console.WriteLine($"When added : {firstNumber + secondNumber}");
+}
+
+static void SubtractingMethod(int firstNumber, int secondNumber)
+{
+    Console.WriteLine($"When subtracted : {firstNumber - secondNumber}");
+}
+
+static void MultipleyingMethod(int firstNumber, int secondNumber)
+{
+    Console.WriteLine($"When multiplied : {firstNumber * secondNumber}");
+}
+
+static void DividingMethod(int firstNumber, int secondNumber)
+{
+    Console.WriteLine($"When divided : {firstNumber / secondNumber}");
+}
+
+static void SquareRootMethod(int firstNumber, int secondNumber)
+{
+    Console.WriteLine($"When rooting: The square root of {firstNumber} is {Math.Sqrt(firstNumber)}");
+    Console.WriteLine($"When rooting: The square root of {secondNumber} is {Math.Sqrt(secondNumber)}");
+}
+
+static void PowMethod(int firstNumber, int secondNumber)
+{
+    Console.WriteLine($"The square and cube of the '{firstNumber}' : {Math.Pow(firstNumber, 2)} , {Math.Pow(firstNumber, 3)}");
+    Console.WriteLine($"The square and cube of the '{secondNumber}' : {Math.Pow(secondNumber, 2)} , {Math.Pow(secondNumber, 3)}");
 }
